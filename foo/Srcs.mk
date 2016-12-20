b4d85727bed14c67744c7819f3ae9d8ee9f4cda6
Max Gautier <mgautier@student.42.fr>
2016-12-20 12:42:12 +0100

    One more test file

--- foo/Srcs.mk
+++ foo/Srcs.mk
@@ -6,12 +6,12 @@
 #    By: mgautier <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/12/19 07:58:53 by mgautier          #+#    #+#              #
-#*   Updated: 2016/12/19 14:46:06 by mgautier         ###   ########.fr       *#
+#*   Updated: 2016/12/20 12:13:34 by mgautier         ###   ########.fr       *#
 #                                                                              #
 # **************************************************************************** #
 
 TARGET := test.a
-SRC := test_2.c
+SRC := test_2.c test_3.c
 
 # Dependencies
 
